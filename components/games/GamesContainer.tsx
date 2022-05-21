13daba80fb7f2fe136c1ba4801d64e61c05741b2
Aleksander Petrov <apetrovdev@gmail.com>
2022-05-21 09:31:18 +0300

    example conditional rendering for user roles

--- components/games/GamesContainer.tsx
+++ components/games/GamesContainer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import GameList from "./GameList";
 import { useGamesQuery, useDeleteGameMutation } from "../../generated/graphql";
+import {queryClient} from "../../pages/_app";
 
 const GamesContainer: React.FC = () => {
 
+    const currentUserQuery: any = queryClient.getQueryData('CurrentUser')
     const { isLoading, isError, data, error, refetch } = useGamesQuery({})
     const { mutate} = useDeleteGameMutation({
       onSuccess: () => {
@@ -18,12 +20,22 @@ const GamesContainer: React.FC = () => {
     if (isLoading) {
       return <span>Loading...</span>
     }
-  
+
     if (error) {
-      return <span>Error: {error?.message}</span>
+      return <span>{error?.message}</span>
+    }
+    const getUserRoles = () => {
+        const userRoles = currentUserQuery?.currentUser?.roles
+        if(!userRoles) {
+            return []
+        }
+        return userRoles
     }
     return (
         <div>
+            {getUserRoles().includes('USER') && <span>User</span>}
+            {getUserRoles().includes('ADMIN') && <span>Admin</span>}
+            {getUserRoles().includes('SUPER_ADMIN') && <span>Super admin</span>}
             {data && <GameList games={data.games} onDelete={onDelete}/>}
         </div>
     )
